28e623f16ea4a78de6467ed98fc6819be82026f6
Mato6t <58171380+mato6t@users.noreply.github.com>
2020-02-17 10:03:56 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,15 @@ class ProjectToTry(MycroftSkill):
 
     @intent_file_handler('try.to.project.intent')
     def handle_try_to_project(self, message):
+        txtFile = open('vystup.txt','w')
+        txtFile.write("dobre, ide to")
+        txtFile.close()
         self.speak_dialog('try.to.project')
+
+
+
+    def stop(self):
+        pass
 
 
 def create_skill():
